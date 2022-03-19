da2b8bbca325d555c5ca5735960023635cbaf47d
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-03-19 15:06:53 +0330

    Edit Public Path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? "/reponame/" : "/"
+  publicPath: process.env.NODE_ENV === 'production' ? "/VueMovify/" : "/"
 })
