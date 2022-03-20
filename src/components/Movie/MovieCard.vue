378ab62e7fe224589a789dff2bfa87e6283bf645
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-03-20 09:52:01 +0330

    add some styles

--- src/components/Movie/MovieCard.vue
+++ src/components/Movie/MovieCard.vue
@@ -4,7 +4,10 @@
     <img :src="props.imgUrl" alt="null" class="card__image" @click="imgClicked">
 
     <div class="card__content">
-
+      <div class="card__info">
+        <h4 class="info__title">NeoFilm</h4>
+        <p class="info__rate">2.4 / 10</p>
+      </div>
     </div>
 
   </div>
@@ -21,11 +24,33 @@ function imgClicked() {
 </script>
 
 <style lang="scss" scoped>
-
 .card {
-  width: 6rem;
-  height: 2rem;
-  background: blue;
+  width: 30%;
+  background: white;
+  box-shadow: 0 4px 0.5rem 0.1rem rgba(0, 0, 0, 0.15);
+  border-radius: 1rem;
+  overflow: hidden;
+
+  .card__image {
+    width: 100%;
+  }
+
+  .card__content {
+    padding: 0.5rem;
+
+    .card__info {
+      display: flex;
+      flex-direction: row;
+
+      .info__title {
+        flex-basis: 70%;
+      }
+      .info__rate {
+        flex-basis: 30%;
+        text-align: end;
+      }
+    }
+  }
 }
 
 </style>
