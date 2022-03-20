7371a46524215f140cada786ebb8faf3b11181b8
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-03-20 08:20:16 +0330

    import splidejs theme css

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import '@/assets/styles/all.scss'
+import '@splidejs/splide/dist/css/themes/splide-skyblue.min.css'
 import { createPinia } from 'pinia'
 
 createApp(App)
