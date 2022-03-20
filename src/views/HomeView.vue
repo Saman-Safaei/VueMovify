430ea9d1d9779f306f7b64aef405655fb832f0b8
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-03-20 10:58:15 +0330

    add cards to HomeView for test card styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,29 @@
 <template>
   <TopHeader />
-  <div style="min-height: 2000px"></div>
+  <div class="movie-container">
+    <movie-card img-url="http://moviesapi.ir/images/tt0111161_poster.jpg"></movie-card>
+    <movie-card img-url="http://moviesapi.ir/images/tt0111161_poster.jpg"></movie-card>
+    <movie-card img-url="http://moviesapi.ir/images/tt0111161_poster.jpg"></movie-card>
+    <movie-card img-url="http://moviesapi.ir/images/tt0111161_poster.jpg"></movie-card>
+    <movie-card img-url="http://moviesapi.ir/images/tt0111161_poster.jpg"></movie-card>
+    <movie-card img-url="http://moviesapi.ir/images/tt0111161_poster.jpg"></movie-card>
+    <movie-card img-url="http://moviesapi.ir/images/tt0111161_poster.jpg"></movie-card>
+  </div>
 </template>
 
 <script setup>
 import TopHeader from "@/components/Header/TopHeader.vue";
+import MovieCard from "@/components/Movie/MovieCard.vue";
 </script>
+
+<style lang="scss" scoped>
+
+.movie-container {
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: 31% 31% 31%;
+  padding: 1rem;
+  justify-content: space-between;
+}
+
+</style>
