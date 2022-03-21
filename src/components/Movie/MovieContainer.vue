26bc542f8ac2e910f5811a6342fc929e4969a167
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-03-21 19:10:09 +0330

    add styles and templates

---
+++ src/components/Movie/MovieContainer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/base/variables';
+
+.container {
+  display: grid;
+  grid-template-columns: auto;
+
+  @media screen and (min-width: $sm) {
+    grid-template-columns: auto auto;
+  }
+
+  @media screen and (min-width: $md) {
+    grid-template-columns: auto auto auto;
+  }
+
+  @media screen and (min-width: $lg) {
+    grid-template-columns: auto auto auto auto;
+  }
+}
+</style>
