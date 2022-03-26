fe14e75b5145cb4d431b080d82a510c2f6d90b82
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-03-26 21:19:29 +0430

    title and styles added

--- src/views/ErrorView.vue
+++ src/views/ErrorView.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="view error-view"></div>
+  <div class="view error-view">
+    <h3 class="error__title">Page Not Found - Error Code : 404</h3>
+  </div>
 </template>
 
 <script setup>
@@ -7,5 +9,15 @@
 </script>
 
 <style lang="scss" scoped>
+.error-view {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 85vh;
 
+  .error__title {
+    font-weight: normal;
+    font-size: 1.3rem;
+  }
+}
 </style>
