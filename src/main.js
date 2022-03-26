d01301aa6e562475451a2ec851ec0bd89337c903
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-03-26 21:52:52 +0430

    pinia initialize before router

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import '@/assets/styles/all.scss'
 import '@splidejs/splide/dist/css/themes/splide-skyblue.min.css'
 import { createPinia } from 'pinia'
 
+const pinia = createPinia();
+
 createApp(App)
+    .use(pinia)
     .use(router)
-    .use(createPinia())
     .mount('#app');
