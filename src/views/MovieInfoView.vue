7d36df407cfdbfdb309a80ff9d22f03391313caa
Samuel <xsamansafaeix@gmail.com>
2022-04-07 12:27:39 +0430

    new view added 'MovieInfoView'

---
+++ src/views/MovieInfoView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="view movie-info">
+      <p>this is a text</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.movie-info {
+    padding: 1rem;
+}
+</style>
