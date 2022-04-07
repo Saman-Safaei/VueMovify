173ad4da5635731ec743f2de5787098443e3038d
Samuel <xsamansafaeix@gmail.com>
2022-04-07 11:51:37 +0430

    fix column sizes for grid layout 'change column sizes to 1fr'

--- src/components/Movie/MovieContainer.vue
+++ src/components/Movie/MovieContainer.vue
@@ -9,24 +9,24 @@
 
 .container {
   display: grid;
-  grid-template-columns: auto;
+  grid-template-columns: repeat(1, minmax(0, 1fr));
   padding: 1rem;
   gap: 1rem;
 
   @media screen and (min-width: $sm) {
-    grid-template-columns: auto auto;
+    grid-template-columns: repeat(2, minmax(0, 1fr));
   }
 
   @media screen and (min-width: $md) {
-    grid-template-columns: auto auto auto;
+    grid-template-columns: repeat(3, minmax(0, 1fr));
   }
 
   @media screen and (min-width: $lg) {
-    grid-template-columns: auto auto auto auto;
+    grid-template-columns: repeat(4, minmax(0, 1fr));
   }
 
   @media screen and (min-width: $xl) {
-    grid-template-columns: auto auto auto auto auto;
+    grid-template-columns: repeat(5, minmax(0, 1fr));
   }
 }
 </style>
